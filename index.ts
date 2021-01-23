6655a2a34de7bc960986998263392f87c3b7cfe0
shrinivasshah <shrinivasshah05@gmail.com>
2021-01-23 08:19:18 +0530

    added types on each param in every fuction

--- index.ts
+++ index.ts
@@ -3,19 +3,28 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+} //interfaces in typescript are used to define structure of an object
+
 axios.get(url).then((res) => {
-  const todo = (res.data)
+  const todo = (res.data) as Todo
 
-  const ID = todo.ID;
-  const title = todo.Title;
-  const finished = todo.finished;
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
 
 
-  console.log(`
-  The Todo with ID: ${ID}
-  Has a title of ${title}
-  Is it finished ${finished}
-  `);
-  
+  logTodo(id,title,completed )
 });
 
+const logTodo = (id: number, title: string, completed:boolean) =>{
+  console.log(`
+  The Todo with ID: ${id}
+  Has a title of ${title}
+  Is it finished ${completed}
+  `);
+  
+}
